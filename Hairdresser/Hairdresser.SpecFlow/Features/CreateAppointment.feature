d47e40ec17bc697619ec1e285b5cae28ee351383
René Jørgensen <rene686f@easv365.dk>
2022-12-29 13:27:24 +0100

    Update

---
+++ Hairdresser/Hairdresser.SpecFlow/Features/CreateAppointment.feature
@@ -0,0 +1,29 @@
+﻿Feature: CreateAppointment
+in order to create an assignment
+i want to make sure that the assignment date
+is not longer than a year in the future
+and that my assignment date is not set in the past
+
+@tag1
+Scenario: Create appointment succesfully
+	Given the appointment date is <assignmentDate_AddDays>
+	When we call CreateAppointment
+	Then the outcome should be <result>
+	  
+
+	Examples: 
+	| assignmentDate_AddDays | result |
+	| 1                      | true   |
+	| 10                     | true   |
+	| 100                    | true   |
+
+Scenario: Create appointment unsuccesfully
+	Given the appointment date is <assignmentDate_AddDays>
+	When we call CreateAppointment
+	Then the outcomse should throw an <error>
+
+	Examples: 
+	| appointmentDate_AddDays | error |
+	| -1                      | 'Tidspunkt for behandling kan ikke være i fortiden' |
+	| 366                     | 'Du kan højest booke din tid et år frem' |
+	| 500                     | 'Du kan højest booke din tid et år frem' |
---
+++ Hairdresser/Hairdresser.SpecFlow/StepDefinitions/CreateAppointmentStepDefinitions.cs
@@ -0,0 +1,72 @@
+﻿using Hairdresser.Core.Application.Implementation;
+using Hairdresser.Core.Application.Interfaces;
+using Hairdresser.Core.Domain.Interfaces;
+using Hairdresser.Core.Entities;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Hairdresser.SpecFlow.StepDefinitions
+{
+    [Binding]
+    public class CreateAppointmentStepDefinitions
+    {
+        
+        private readonly Mock<IAppointmentRepository> appointmentRepo;
+        
+
+        Appointment appointment = new Appointment();
+        Appointment resultAppointment = new Appointment();
+
+        public CreateAppointmentStepDefinitions()
+        {
+            appointmentRepo = new Mock<IAppointmentRepository>();
+            
+        }
+
+        // Standard appointment we can call and use for our tests
+        private void SetAppointment()
+        {
+            appointment = new Appointment()
+            {
+                AppointmentDateTime = DateTime.Today,
+                CustomerID = 1,
+                EmployeeID = 1,
+                Status = 1,
+                TreatmentID = 1,
+            };
+        }
+
+        [Given(@"the appointment date is (.*)")]
+        public void GivenTheAppointmentDateIsAssignmentDate_AddDays(int value)
+        {
+            SetAppointment();
+            appointment.AppointmentDateTime = DateTime.Today.AddDays(value);
+        }
+
+        [When(@"we call CreateAppointment")]
+        public void WhenWeCallCreateAppointment()
+        {
+            var service = new AppointmentService(appointmentRepo.Object);
+            resultAppointment = service.Create(appointment);
+            Console.WriteLine(resultAppointment.ToString());
+        }
+
+        [Then(@"the outcome should be (.*)")]
+        public void ThenTheOutcomeShouldBeTrue(bool result)
+        {
+            bool isSame = false;
+            if(resultAppointment is Appointment)
+            {
+                isSame = true;
+                
+            }
+            Assert.Equal(isSame, result);
+        }
+
+    }
+}
